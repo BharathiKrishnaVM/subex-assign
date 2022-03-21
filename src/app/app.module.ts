83569b2808afeaa8f6b4513acf1fb1b60a7fe342
Yashika186 <49585360+BharathiKrishnaVM@users.noreply.github.com>
2022-03-21 11:17:19 +0530

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,43 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { CalendarModule } from 'primeng/calendar';
+import { SliderModule } from 'primeng/slider';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { UsersService } from './users.service';
+import { AddUserComponent } from './add-user/add-user.component';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { RouterModule } from '@angular/router';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
-    AppComponent
+   AppComponent,
+   NavBarComponent,
+   HomeComponent,
+   AddUserComponent,
+   UserListComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    ButtonModule,
+    TableModule,
+    CalendarModule,
+    SliderModule,
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
